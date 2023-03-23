0a6d3e72ae92c79c95ae3e380c2d17e413e8c4ff
FEvziTaskin <fevzitaskin28@gmail.com>
2023-03-23 23:51:11 +0100

    Sidebar Project with custom hook uploaded

---
+++ 20-Sidebar Modal (Context API - Custom Hooks)/README.md
@@ -0,0 +1,8 @@
+---
+
+There are two ways to use useContext
+
+1 - useContext hook
+2 - custom hooks (by using useContext Hook)
+
+I will be leaving the two options for your reference.
---
+++ 20-Sidebar Modal (Context API - Custom Hooks)/src/Home.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { FaBars } from "react-icons/fa";
+import { AppContext, useGlobalContext } from "./context";
+
+const Home = () => {
+  // const data = useContext(AppContext); ==> useContext
+  const { openSidebar, openModal } = useGlobalContext(); // ==> custom hooks
+
+  return (
+    <main>
+      <button className="sidebar-toggle" onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
+    </main>
+  );
+};
+
+export default Home;
