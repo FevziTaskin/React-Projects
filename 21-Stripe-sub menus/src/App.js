9123539c7cb708adce5245096e820aa00f551b36
FEvziTaskin <fevzitaskin28@gmail.com>
2023-03-25 00:56:50 +0100

    Stripe submenus uploaded

---
+++ 21-Stripe-sub menus/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Hero from "./Hero";
+import Submenu from "./Submenu";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
+  );
+}
+
+export default App;
